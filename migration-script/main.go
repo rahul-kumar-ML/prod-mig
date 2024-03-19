472a295a677bfc5850c966d4918501c3f950630e
rahul-kumar-ML <135819739+rahul-kumar-ML@users.noreply.github.com>
2024-03-19 16:28:20 +0530

    Update main.go

--- migration-script/main.go
+++ migration-script/main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"encoding/json"
 	"gorm.io/gorm"
+	"fmt"
 )
 
 func updateDatabaseAzure(db *gorm.DB, sink KConnectSink) error {
 	containerName := "archive-" + sink.ProfileID + "-profile"
-	// Define a map to represent the update data
-	updateData := map[string]interface{}{
-		"format.class":                   "io.confluent.connect.azureblob.format.orc.OrcFormat",
-		"storage.class":                  "io.confluent.connect.azureblob.storage.AzureBlobStorage",
-		"connector.class":                "io.confluent.connect.azureblob.AzureBlobSinkConnector",
-		"azureblob.account.key":          "JWk+PUNVStyk3ogVowgxxJRZVhPwxEFP4ffzABm74u2ZlA4eMICmgNmQw+94udrpGZTHmsXlwzE1+AStBYj7nw==",
-		"azureblob.container.name":       containerName,
-		"azureblob.storage.account.name": "apmmanagerstorageacc",
-		"azureblob.block.size":           5242880,
+	
+	// Unmarshal the JSON data into a map
+	var configMap map[string]interface{}
+	if err := json.Unmarshal([]byte(sink.Config), &configMap); err != nil {
+		return err
 	}
+	// Update the specific fields in the map
+	configMap["format.class"] = "io.confluent.connect.azureblob.format.orc.OrcFormat"
+	configMap["storage.class"] = "io.confluent.connect.azureblob.storage.AzureBlobStorage"
+	configMap["connector.class"] = "io.confluent.connect.azureblob.AzureBlobSinkConnector"
+	configMap["azureblob.account.key"] = "newAccountKey"
+	configMap["azureblob.container.name"] = containerName
+	configMap["azureblob.storage.account.name"] = "apmmanagerstorageacc"
+	configMap["azureblob.block.size"] = 5242880
 
 	// Convert updateData to JSON
-	updateJSON, err := json.Marshal(updateData)
+	updateJSON, err := json.Marshal(configMap)
 	if err != nil {
 		return err
 	}
@@ -40,7 +45,10 @@ func main() {
 	db.Model(&KConnectSink{}).Where("name LIKE ?", "s3%").Find(&sinks)
 	if config.CloudType == "Azure" {
 		for _, sink := range sinks {
-			updateDatabaseAzure(db, sink)
+			err := updateDatabaseAzure(db, sink)
+			if err != nil {
+				fmt.Printf("unable to update db for id=?",sink.ID)
+			}
 		}
 	} else if config.CloudType == "AWS" {
 		//cloud specific function call
