8e13459cef34eae91a442fa50d0d8611310731f2
rahul-kumar-ML <135819739+rahul-kumar-ML@users.noreply.github.com>
2024-03-19 16:44:18 +0530

    Update schema.go

--- migration-script/schema.go
+++ migration-script/schema.go
@@ -6,17 +6,17 @@ func (KConnectSink) TableName() string {
 }
 
 type KConnectSink struct {
-	ID              uint `gorm:"primary_key"`
-	Name            string
-	ProfileID       string
-	Type            string
-	ConnectURL      string
-	MaxTasks        int
-	Config          string `gorm:"type:jsonb"`
-	State           string
-	TrueState       string
-	LastKnownError  string
-	UserManaged     bool
-	ByteRateQuota   int
-	QuotaUsageStats string
+	ID              uint            `gorm:"primary_key"`
+	Name            string          `gorm:"column:name"`
+	ProfileID       string          `gorm:"column:profileid"`
+	Type            string          `gorm:"column:type"`
+	ConnectURL      string          `gorm:"column:connect_url"`
+	MaxTasks        int             `gorm:"column:max_tasks"`
+	Config          json.RawMessage `gorm:"type:jsonb;column:config"`
+	State           string          `gorm:"column:state"`
+	TrueState       string          `gorm:"column:true_state;default:RUNNING"`
+	LastKnownError  string          `gorm:"column:last_known_error;default:''"`
+	UserManaged     bool            `gorm:"column:user_managed;default:false"`
+	ByteRateQuota   int             `gorm:"column:byte_rate_quota"`
+	QuotaUsageStats json.RawMessage `gorm:"type:jsonb;column:quota_usage_stats"`
 }
